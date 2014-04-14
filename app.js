f5001eb318f77910c0ff7065dfa398440d9ce1e6
Dan Heidel <dan.heidel@gmail.com>
2014-04-13 23:44:26 -0700

    refactored for env variables

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+'use strict';
 
+var user, port;
+startup();
+
+var express = require('express');
+
+var server = express(); // better instead
+server.use('/media', express.static(__dirname + '/media'));
+server.use(express.static(__dirname + '/public'));
+
+server.listen(port);
+console.log('server started at: ' + port);
+
+function startup(){
+  console.log('starting as user: ' + process.env.USER);
+
+  user = parseInt(process.env.NODEUSERID) || parseInt(process.argv[2]);
+  if(!user){
+    console.error('no user specified, exiting');
+    process.exit();
+  }
+
+  //attempt to de-escalate user permissions
+  try {
+    process.setgid(user);
+    process.setuid(user);
+  } catch (e) {
+    console.error('problem setting user/group, exiting');
+    console.dir(e);
+    process.exit();
+  }
+  console.log('user changed to: ' + user);
+
+  port = parseInt(process.env.NODESERVERPORT) || parseInt(process.argv[3]);
+  if(!port){
+    console.error('no port defined, exiting');
+    process.exit();
+  }
+}
