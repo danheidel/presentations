f85b3bc8d05f9444c19943849c0849e7ac3065b3
Dan Heidel <dan.heidel@gmail.com>
2014-04-14 21:41:24 -0700

    rearranged directories and setup alternate index.htmls

--- app.js
+++ app.js
@@ -3,14 +3,48 @@
 var user, port;
 startup();
 
+var fs = require('fs');
 var express = require('express');
+var consolidate = require('consolidate');
 
 var server = express(); // better instead
-server.use('/media', express.static(__dirname + '/media'));
+server.use(express.static(__dirname));
 server.use(express.static(__dirname + '/public'));
+parseSlidesDir('slides', setupRoutes);
+// server.use('/test', express.static(__dirname + '/slides/test'));
+// server.use('/test', express.static(__dirname + '/public'));
 
 server.listen(port);
 console.log('server started at: ' + port);
+
+function parseSlidesDir(startDir, doneCb){
+  var routes = [];
+  fs.readdir(startDir, function(err, files){
+    if(err){ return doneCb(err);}
+    var numFiles = files.length;
+    if(!numFiles) { return doneCb('empty directory');}
+    files.forEach(function(file){
+      fs.stat(startDir + '/' + file, function(err, stats){
+        if(stats && stats.isDirectory()){
+          routes.push(file);
+        }
+        numFiles --;
+        if(numFiles === 0){
+          return doneCb(null, routes);
+        }
+      });
+    });
+  });
+}
+
+function setupRoutes(err, routes){
+  console.log(err);
+  routes.forEach(function(route){
+    console.log(route);
+    server.use('/' + route, express.static(__dirname + '/slides/' + route));
+    server.use('/' + route, express.static(__dirname + '/public'));
+  });
+}
 
 function startup(){
   console.log('starting as user: ' + process.env.USER);
