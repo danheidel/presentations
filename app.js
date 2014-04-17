7164d851863bd08e05b4cd2560d956394952d467
Dan Heidel <dan.heidel@gmail.com>
2014-04-17 13:33:55 -0700

    setup slide libraries, created menu

--- app.js
+++ app.js
@@ -5,44 +5,72 @@ startup();
 
 var fs = require('fs');
 var express = require('express');
+var server = express();
 var consolidate = require('consolidate');
+var handlebars = require('handlebars');
 
-var server = express(); // better instead
-server.use(express.static(__dirname));
-server.use(express.static(__dirname + '/public'));
-parseSlidesDir('slides', setupRoutes);
-// server.use('/test', express.static(__dirname + '/slides/test'));
-// server.use('/test', express.static(__dirname + '/public'));
+server.engine('html', consolidate.handlebars);
+server.set('view engine', 'html');
+server.set('views', __dirname + '/views');
 
-server.listen(port);
-console.log('server started at: ' + port);
+parseSlidesDir('slides', function(err, routes){
+  if(err){
+    console.error('directory parsing error: ' + err);
+    process.exit();
+  }
+  startServer(routes);
+});
+
+function startServer(slideRoutes){
+  server.get('/', function(req, res){
+    res.render('index', {
+      routes: slideRoutes
+    });
+  });
+  server.use(express.static(__dirname + '/public'));
+  slideRoutes.forEach(function(route){
+    console.log(route);
+    server.use('/' + route.route, express.static(__dirname + '/slides/' + route.route));
+    server.use('/' + route.route, express.static(__dirname + '/public'));
+  });
+
+  server.listen(port);
+  console.log('server started at: ' + port);
+}
+
+function registerPartials(path){
+
+}
 
 function parseSlidesDir(startDir, doneCb){
   var routes = [];
   fs.readdir(startDir, function(err, files){
     if(err){ return doneCb(err);}
     var numFiles = files.length;
-    if(!numFiles) { return doneCb('empty directory');}
-    files.forEach(function(file){
-      fs.stat(startDir + '/' + file, function(err, stats){
+    if(!numFiles || numFiles === 0) { return doneCb('empty directory');}
+    files.forEach(function(directory){
+      fs.stat(startDir + '/' + directory, function(err, stats){
         if(stats && stats.isDirectory()){
-          routes.push(file);
-        }
-        numFiles --;
-        if(numFiles === 0){
-          return doneCb(null, routes);
+          fs.readFile(startDir + '/' + directory + '/info.json', function(err, data){
+            if(err){
+              return doneCb('info.json config file for ' + directory + ' missing');
+            }
+            var tempRoute = JSON.parse(data);
+            tempRoute.route = directory;
+            routes.push(tempRoute);
+            numFiles --;
+            if(numFiles === 0){
+              return doneCb(null, routes);
+            }
+          });
+        } else {
+          numFiles --;
+          if(numFiles === 0){
+            return doneCb(null, routes);
+          }
         }
       });
     });
-  });
-}
-
-function setupRoutes(err, routes){
-  console.log(err);
-  routes.forEach(function(route){
-    console.log(route);
-    server.use('/' + route, express.static(__dirname + '/slides/' + route));
-    server.use('/' + route, express.static(__dirname + '/public'));
   });
 }
 
